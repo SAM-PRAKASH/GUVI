45461897d667d36b204da3430012dbb7b377d935
SAM-PRAKASH <98145168+SAM-PRAKASH@users.noreply.github.com>
2023-06-13 10:22:23 +0530

    codekata_june13

---
+++ plus one.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+ let a = [];
+var plusOne = function(digits) {
+    let value = BigInt(digits.join('')) + 1n;
+
+    return value.toString().split('');
+};
